e1a3f37cae82d0dd077d959ad1c6e8bfb514c068
piguycs <piguycs@pm.me>
2025-11-02 22:03:34 +0100

    a slightly more complex example

--- games/simulation/src/main.cpp
+++ games/simulation/src/main.cpp
@@ -6,15 +6,24 @@ struct Score {
     Score(int value): value(value) {}
 };
 
-void printScore(Score& score) {
-    printf("THE SCORE IS: %d\n", score.value);
+struct PlayerId {
+    int id = 10;
+};
+
+void printScore(Score& score, PlayerId& id) {
+    printf("THE SCORE FOR %d IS: %d\n", id.id, score.value);
     score.value += 10;
 }
+
+void renderFps() { raylib::DrawFPS(10, 10); }
 
 int main() {
     engine::Engine engine;
 
     engine.registerResource<Score>(10);
+    engine.registerResource<PlayerId>();
+
+    engine.addSystem(renderFps);
     engine.addSystem(printScore);
 
     engine.start();
